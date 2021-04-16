afa72df5795841ede4318e13f7c1a01740a1c38d
khargr14 <kay_hargrove@yahoo.com>
2021-04-16 00:18:06 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,5 +1,13 @@
 export default function manageTodo(state = {
-  todos: [],
+  toexport default function manageTodo(state = {
+    todos: [],
+  }, action) {
+  
+    console.log("reducer received this action:", action);
+  
+    return state;
+  }
+  dos: [],
 }, action) {
 
   console.log("reducer received this action:", action);
